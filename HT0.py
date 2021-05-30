c250ac6ace01b39d566626cceaebf114c4f153f3
Ronaldo <ronaldoposadas0o@gmail.com>
2021-05-29 19:53:14 -0600

    Prueba

--- HT0.py
+++ HT0.py
@@ -2,3 +2,4 @@ masa = float(input("Ingresa tu peso en kg: "))
 altura = float(input("Ingresa tu Estatura en m: "))
 indice = float((masa / altura**2))
 print(f"Tu indice de masa corporal es: {round(indice, 2)}")
+#QUIERO HACER UNA PRUEBA
